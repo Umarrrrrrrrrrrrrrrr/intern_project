99af13de390860bf27d03b6b30b1e44126740887
Umarrrrrrrrrrrrrrrr <np05cp4a230043@iic.edu.np>
2025-07-23 10:22:35 +0545

    InventoryController

--- src/main/java/org/umar/learning/model/Category.java
+++ src/main/java/org/umar/learning/model/Category.java
@@ -3,6 +3,7 @@ package org.umar.learning.model;
 import jakarta.persistence.*;
 import java.util.List;
 
+@Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
