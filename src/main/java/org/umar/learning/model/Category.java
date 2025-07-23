6d8e9f99bf17053291b07a1abf23724e4a1ffc13
Umarrrrrrrrrrrrrrrr <np05cp4a230043@iic.edu.np>
2025-07-23 20:38:38 +0545

    repaired category

--- src/main/java/org/umar/learning/model/Category.java
+++ src/main/java/org/umar/learning/model/Category.java
@@ -1,6 +1,8 @@
 package org.umar.learning.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
 @Entity
@@ -8,28 +10,32 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
 
     //optional : one category can have many inventory
-    @OneToMany(mappedBy = "Category", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Inventory> inventories;
 
-    public Category(long id, String name) {}
-        public long getId() {
-            return id;
-        }
+    public Category() {}
 
-        public void setId(long id) {
-            this.id = id;
-        }
+    public Category(String name) {
+        this.name = name;
+    }
 
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+         return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
