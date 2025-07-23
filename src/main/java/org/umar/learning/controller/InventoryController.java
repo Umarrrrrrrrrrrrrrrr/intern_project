3ba79c35fec4100dd92662d7de58799cdf24f185
Umarrrrrrrrrrrrrrrr <np05cp4a230043@iic.edu.np>
2025-07-23 23:34:13 +0545

    adding inventory apis and stuffs

--- src/main/java/org/umar/learning/controller/InventoryController.java
+++ src/main/java/org/umar/learning/controller/InventoryController.java
@@ -1,4 +1,36 @@
 package org.umar.learning.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.umar.learning.model.Inventory;
+import org.umar.learning.service.InventoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping
+    public List<Inventory> getAll() {
+        return inventoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Inventory> getById(@PathVariable Long id) {
+        return inventoryService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Inventory create(@RequestBody Inventory inventory) {
+        return inventoryService.save(inventory);
+    }
 }
